cd3691148899b3006cb59476ec6ebb887b915445
Alexander Eberspächer <alex.eberspaecher@gmail.com>
2012-02-27 15:05:05 +0100

    Update a preliminary version of the script.

--- pbsjob.py
+++ pbsjob.py
@@ -6,6 +6,7 @@
 
 The name of the login node is read from a plain text file (~/pbsjob.dat).
 The working directory on the remote machine is also given in that file.
+Furthermore, a suffix for jobscripts may as well be given.
 Modifictations to the file's name and location have to be performed in the
 source code.
 
@@ -18,6 +19,8 @@ line options. Finally, the jobscript will be submitted.
 
 from optparse import OptionParser
 from subprocess import Popen
+import subprocess
+import tempfile
 import os, sys
 
 # parse command line options:
@@ -52,7 +55,7 @@ if(len(args) == 0):
 # obtain login information from file:
 settingsDirectory = os.environ["HOME"]
 settingsFileName = "pbsjob.dat"
-# The file shall contain a single line: user@login.machine.tld
+# The file shall contain the line: user@login.machine.tld
 try:
     if(settingsDirectory[-1] != "/"):
         settingsDirectory += "/"
@@ -62,20 +65,33 @@ except:
     sys.exit(1)
 
 try:
-    login = fil.readline()
+    login = fil.readline().strip()
 except:
     print >> sys.stderr, "Could not read host from %s"%fil
 
 try:
-    workdir = fil.readline()
+    workdir = fil.readline().strip()
 except:
     workdir = os.environ["PWD"]
     print("No working directory specified, using %s instead."%workdir)
+
+try:
+    suffix = fil.readline().strip()
+except:
+    suffix = ".jobscript"
+    print("No suffix for jobscript given, using suffix %s instead."%suffix)
+    
 # catch the case of workdir being an empty line (may happen with text
 # editors that automatically append a new line at the end of each file)
 if(workdir == ""):
     workdir = os.environ["PWD"]
     print("No working directory specified, using %s instead."%workdir)
+
+if(suffix == ""):
+    suffix = ".jobscript"
+    print("No suffix for jobscript given, using suffix %s instead."%suffix)
+
+# TODO: try to remove that annoying code duplication above
 
 fil.close()
 
@@ -92,11 +108,28 @@ if(not options.name):
 else:
     jobName = options.name
 
-if(options.shared):
+if(options.shared): # if shared is used, prepare a suitable string for jobscript
     sharedString = "#shared"
 else:
     sharedString = ""
 
+# now try to find out if the script/program to execute already exits in the
+# remote directory - if so, prompt before it is overwritten
+devnull = open(os.devnull) # also used later!
+errcode = subprocess.call(["ssh", login, "ls", "%s/%s"%(workdir, args[0])],
+                          stdout=devnull, stderr=devnull)
+
+if(errcode == 0): # file already exists, ask to overwrite
+    decision = raw_input("The file %s already exists in the remote location!\nOverwrite [y/n]? "%args[0])
+    if(decision not in ["y", "Y", "yes", "Yes", "YES"]):
+        sys.exit(0)
+
+errcode = subprocess.call(["scp", args[0], "%s:%s"%(login,workdir)],
+                          stdout=devnull, stderr=devnull)
+if(errcode != 0):
+    print("Something went wrong copying the program to be executed! Aborting!")
+    sys.exit(1)
+    
 # now generate a jobscript:
 # We assume OpenMPI in recent versions - in these versions, it is unnecessary
 # to specify the hostnames and number of processes
@@ -108,7 +141,8 @@ r"""#!/bin/sh
 #PBS -o %s
 #PBS -e %s
 ### Number of nodes, PPN, shared
-#PBS - l nodes=%s:%s%s, walltime=%s:00:00
+#PBS -l nodes=%s:%s%s
+#PBS -l walltime=%s:00:00
 
 . $HOME/.bashrc
 
@@ -121,7 +155,26 @@ mpirun %s
 """\
 %(jobName, stdoutFile, stderrFile, options.nodes, options.ppn, sharedString, options.walltime, args[0])
 
-print jobscript
+# write jobscript to a temporary file:
+jobFile = tempfile.NamedTemporaryFile(suffix=suffix, dir="")
+jobFile.write(jobscript)
+jobFile.flush()
 
-# connect to login node:
+# scp the jobscript to the remote
+print("Copy jobscript to %s:%s"%(login, workdir))
+errcode = subprocess.call(["scp", "%s"%jobFile.name, "%s:%s"%(login, workdir)],
+                stdout=devnull, stderr=devnull)
+if(errcode != 0):
+    print("Copying the jobscript failed, aborting!")
+    sys.exit(1)
+
+# now, qsub the jobscript
+# extract jobscript file's basename
+errcode = subprocess.call(["ssh", login, "cd %s; qsub %s"
+                          %(workdir, jobFile.name.split("/")[-1])])
+if(errcode != 0):
+    print("Something went wrong submitting the jobscript! Aborting!")
+else:
+    print("Job submitted, using %s CPUs in total."%(options.nodes*options.ppn))
+
 
